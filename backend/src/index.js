5b2542152eeb5b07abb1f70d1c1462eac619da40
SimretN <134206815+SimretN@users.noreply.github.com>
2024-09-03 15:10:04 +0200

    create sign up api route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -12,12 +14,22 @@ mongoose.connect(process.env.MONGO)
   console.log(err);
 });
 
-
-
-
-
-
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+
+
+
+
+
+
+
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
