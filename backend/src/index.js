4b96ec14dd980de542c4702e3b9ef7a42cb21a2e
SimretN <134206815+SimretN@users.noreply.github.com>
2024-09-03 16:15:43 +0200

    create a middleware and a function to handle possible errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,15 @@ app.use(express.urlencoded({ extended: true}));
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
-
+app.use((err, req, res, next) => { //middleware
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 
 
