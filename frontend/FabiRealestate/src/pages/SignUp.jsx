0e5d6e155bed98195612a389f8fd785a01de77a2
SimretN <134206815+SimretN@users.noreply.github.com>
2024-09-09 23:18:16 +0200

    adding some changes to signup page

--- frontend/FabiRealestate/src/pages/SignUp.jsx
+++ frontend/FabiRealestate/src/pages/SignUp.jsx
@@ -40,6 +40,7 @@ export default function SignUp() {
     }
   };
   return (
+    <>
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
@@ -81,5 +82,6 @@ export default function SignUp() {
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
+    </>
   );
 }
