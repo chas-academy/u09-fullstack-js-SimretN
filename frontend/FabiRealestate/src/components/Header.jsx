d3167ad0e07196cb81c75d939b830bc771355107
SimretN <134206815+SimretN@users.noreply.github.com>
2024-09-12 23:46:40 +0200

    update the header and make the profile page private

--- frontend/FabiRealestate/src/components/Header.jsx
+++ frontend/FabiRealestate/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import { FaSearch } from 'react-icons/fa';
  import { Link } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { useEffect, useState } from 'react';
+ import { useSelector } from 'react-redux';
+ import { useEffect, useState } from 'react';
 
 export default function Header() {
-  /*const { currentUser } = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState('');
-  const navigate = useNavigate();
+  /*const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
@@ -39,8 +39,8 @@ export default function Header() {
             type='text'
             placeholder='Search...'
             className='bg-transparent focus:outline-none w-24 sm:w-64'
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            /*value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}*/
           />
           <button>
             <FaSearch className='text-slate-600' />
@@ -56,6 +56,7 @@ export default function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
+            
           </Link>
           <Link to='/profile'>
             {currentUser ? (
