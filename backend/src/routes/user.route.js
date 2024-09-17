2458f7a03801580622090adf58f9dde6d0d77c4e
SimretN <134206815+SimretN@users.noreply.github.com>
2024-09-17 22:58:05 +0200

    add delete user functionality

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updatedUser } from '../controllers/user.controller.js';
+import { test, updatedUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../../utils/verifyUser.js';
 
 
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updatedUser)
-
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 
 
