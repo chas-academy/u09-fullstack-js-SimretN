97e293d9c7da444b436b8280d6fedf9addc35216
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-10 11:27:18 +0200

    adding cors to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import cors from "cors"
+// import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -19,6 +21,9 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+  origin: "https://fabiestate.netlify.app/"
+}))
 
 app.use(express.urlencoded({ extended: true}));
 
@@ -27,6 +32,12 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+// app.use(express.static(path.join(__dirname, '/frontend/FabiRealestate/dist')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend/FabiRealestate', 'dist', 'index.html'));
+// })
 
 app.use((err, req, res, next) => { //middleware
   const statusCode = err.statusCode || 500;
