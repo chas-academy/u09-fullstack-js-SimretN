3a0cb0cc26a164f08b53c155c8c7c7b05cfbece5
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-10 11:02:14 +0200

    testing out the proxy is causing issues with the fetch

--- frontend/FabiRealestate/src/pages/Home.jsx
+++ frontend/FabiRealestate/src/pages/Home.jsx
@@ -26,7 +26,8 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        const res = await fetch('/api/listing/get?offer=true&limit=4');
+        //const res = await fetch('/api/listing/get?offer=true&limit=4');
+        const res =  await fetch("https://u09-fullstack-js-simretn.onrender.com/api/listing/get?offer=true&limit=4")
         const data = await res.json();
         setOfferListings(data);
         fetchRentListings();
@@ -66,7 +67,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Fabi Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
