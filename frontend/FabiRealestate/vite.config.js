91af5a29989b97669d922c2d86a2cd8c49dcc82a
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-10 10:52:56 +0200

    changing proxy settings

--- frontend/FabiRealestate/vite.config.js
+++ frontend/FabiRealestate/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       '/api': {
         target: 'https://u09-fullstack-js-simretn.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
