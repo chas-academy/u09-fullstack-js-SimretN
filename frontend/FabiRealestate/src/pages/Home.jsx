b453db78becfa295d3a5d3fcdb460cbabec761eb
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-10 11:15:03 +0200

    changing back to proxy url

--- frontend/FabiRealestate/src/pages/Home.jsx
+++ frontend/FabiRealestate/src/pages/Home.jsx
@@ -26,8 +26,7 @@ export default function Home() {
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
-        //const res = await fetch('/api/listing/get?offer=true&limit=4');
-        const res =  await fetch("https://u09-fullstack-js-simretn.onrender.com/api/listing/get?offer=true&limit=4")
+        const res = await fetch('/api/listing/get?offer=true&limit=4');
         const data = await res.json();
         setOfferListings(data);
         fetchRentListings();
