1a9c938062689021c8e14c5fc92f7a125c2a61f9
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-16 14:28:10 +0200

    add PWA assets and update service worker configuration for offline support

--- frontend/FabiRealestate/vite.config.js
+++ frontend/FabiRealestate/vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
+import manifest from "./public/manifest.json"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +16,14 @@ export default defineConfig({
     },
   },
 
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: manifest,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,svg,png}"]
+      }
+    })
+  ],
 });
