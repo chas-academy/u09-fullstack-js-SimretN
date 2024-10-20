3ff481edd0e374ed19d1f60d682628a9d7b3714a
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-21 00:10:24 +0200

    adding backend url

--- frontend/FabiRealestate/src/components/OAuth.jsx
+++ frontend/FabiRealestate/src/components/OAuth.jsx
@@ -14,7 +14,7 @@ export default function OAuth() {
 
       const result = await signInWithPopup(auth,provider);
 
-      const res = await fetch('/api/auth/google', {
+      const res = await fetch('https://u09-fullstack-js-simretn.onrender.com/api/auth/google', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
