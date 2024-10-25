1b93cc81aec761e8dc1f57eaf0a0cb7c83c7cde8
SimretN <134206815+SimretN@users.noreply.github.com>
2024-10-25 15:19:29 +0200

    adding GET method

--- frontend/FabiRealestate/src/pages/Profile.jsx
+++ frontend/FabiRealestate/src/pages/Profile.jsx
@@ -154,7 +154,7 @@ export default function Profile() {
 
       const res = await fetch(
         `https://u09-fullstack-js-simretn.onrender.com/api/user/listings/${currentUser._id}`,
-        { credentials: "include" }
+        { credentials: "include", method: "GET"}
       );
       const data = await res.json();
       if (data.success === false) {
